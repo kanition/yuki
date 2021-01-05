416c42d55e8745f02972bbd807a94947115e9f96
kanition <68691995+kanition@users.noreply.github.com>
2021-01-05 22:16:47 +0800

    fix VT

--- yuki.cpp
+++ yuki.cpp
@@ -35,24 +35,24 @@ int main(int argc, char const *argv[])
     std::cin >> user_id;
     clean_cin();
     BiliAlbumParser bl(user_id);
-    if (bl.get_user_id().empty())
+    if (!bl.get_user_id().empty())
     {
-        return 1;
+
+        std::cout << "(o°ω°o) 翻翻黑历史? 是否设置时间段? y/[n]" << std::endl;
+        char c = std::cin.get();
+        clean_cin();
+        if (c == 'y' || c == 'Y')
+        {
+            bl.set_time();
+        }
+        std::string save_path;
+        std::cout << "٩(๑>◡<๑)۶ 最后一步啦! 输入保存路径: " << std::endl;
+        std::cin >> save_path;
+        clean_cin();
+        curl_global_init(CURL_GLOBAL_DEFAULT); //全局初始化
+        bl.parse(save_path);
+        curl_global_cleanup();
     }
-    std::cout << "(o°ω°o) 翻翻黑历史? 是否设置时间段? y/[n]" << std::endl;
-    char c = std::cin.get();
-    clean_cin();
-    if (c == 'y' || c == 'Y')
-    {
-        bl.set_time();
-    }
-    std::string save_path;
-    std::cout << "٩(๑>◡<๑)۶ 最后一步啦! 输入保存路径: " << std::endl;
-    std::cin >> save_path;
-    clean_cin();
-    curl_global_init(CURL_GLOBAL_DEFAULT); //全局初始化
-    bl.parse(save_path);
-    curl_global_cleanup();
 #ifdef WIN_OK_H
     if (!color_flag)
     { //恢复显示设置
